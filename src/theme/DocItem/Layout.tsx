0be83dc0f7cba08d6bee0623c7888119be3b0cf3
Rafael Almeida <rafael@iswe.co.nz>
2025-07-05 17:43:23 +1200

    Refactor DocItemLayoutWrapper to remove DevelopmentBanner
    - Simplified the DocItemLayoutWrapper component by removing the DevelopmentBanner.
    - The component now directly renders DocItemLayout, streamlining the layout structure.

--- src/theme/DocItem/Layout.tsx
+++ src/theme/DocItem/Layout.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import DocItemLayout from '@theme-original/DocItem/Layout';
 import type DocItemLayoutType from '@theme/DocItem/Layout';
 import type { WrapperProps } from '@docusaurus/types';
-import DevelopmentBanner from '../../components/DevelopmentBanner';
 
 type Props = WrapperProps<typeof DocItemLayoutType>;
 
 export default function DocItemLayoutWrapper(props: Props): JSX.Element {
-  return (
-    <>
-      <DevelopmentBanner />
-      <DocItemLayout {...props} />
-    </>
-  );
+  return <DocItemLayout {...props} />;
 } 
